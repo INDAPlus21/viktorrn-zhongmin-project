7e926e5a70311fed180b3a0e5a50aa83a39ec85e
viktorrn <90602664+viktorrn@users.noreply.github.com>
2022-05-02 16:07:52 +0200

    made clock tick

--- main/js/main.js
+++ main/js/main.js
@@ -2,6 +2,8 @@ import * as Util from './utility.js';
 const canvas = Util.$('boardScreen');
 const progressbarDOM = Util.$('progressBar');
 const updateRateDOM = Util.$('updatesPerSecond');
+const radiuses = [170,340,510];
+const tileAmounts = [9,18,36];
 //const ctx = canvas.getContext('2d');
 
 
@@ -16,11 +18,11 @@ const updateRateDOM = Util.$('updatesPerSecond');
                                           o//    //=-¨
                                           //     \\
                                          //       \\
-         I        // //O==---====ooooOOOO[o][o][o][o]OOOOOOoooo>>>>--....              // // 
+         I        .o/ //O==---====ooooOOOO[o][o][o][o]OOOOOOoooo>>>>--....              // // 
         //      //    //    \ - /     O     [o]   [o]                [][][]OOoo.......// //--.. 
       {<O>}====||==||[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]}--o
         \\      \\    \\    / - \     O     [o]   [o]                [][][]OOoo¨¨¨¨¨¨¨\\ \\--¨¨  
-         I        \\ \\O==---====ooooOOOO[o][o][o][o]OOOOOOoooo>>>>--¨¨¨¨              \\ \\ 
+         I        ¨*\ \\O==---====ooooOOOO[o][o][o][o]OOOOOOoooo>>>>--¨¨¨¨              \\ \\ 
                                         \\       //
                                          \\     //
                                          o\\    \\=-.
@@ -34,11 +36,33 @@ const updateRateDOM = Util.$('updatesPerSecond');
 
 
 // gamedata that will end up in json
+
 let debugHandler = {
     currentDateTime: new Date(),
     timeSinceLastUpdate: 0,
     frameBuffer: [],
     delta:1,
+
+}
+
+let gameHandler = {
+    layerAngleSpeeds:[],
+    seccondsPerCycle:5,
+    timerValue:0,
+
+    radiuses:[170,340,510],
+    tileAmounts:[9,18,36],
+
+    pause:false,
+    dispFPS:true,
+    players:[],
+    planes: [],
+
+    keyStates:{
+
+    }
+    
+
 }
 
 let tileDimensions = {
@@ -53,38 +77,13 @@ let planeDimensions = {
     height:100,
 }
 
-let BoardDrawingData = {
+let BoardData = {
     innerTiles : [],
     middleTiles: [],
     outerTiles: []
 }
 
-let planes = [
-    {
-        x:0,
-        y:0,
-        radius:510,
-        tilesInCycle:36,
-        angOffset:0,
-        color: "rgba(0, 0, 180"
-    },
-    {
-        x:0,
-        y:0,
-        radius:170,
-        tilesInCycle:1,
-        angOffset:2,
-        color: "rgba(180, 0, 0"
-    },
-    {
-        x:0,
-        y:0,
-        radius:510,
-        tilesInCycle:36,
-        angOffset:3.7,
-        color: "rgba(0, 100, 0"
-    },
-]
+
     
 
 
@@ -94,38 +93,60 @@ let camera = {
 
 // preload stuff
 
+function calcLayerAngleSpeeds(tileAmounts){
+    let list = [];
+    for(let l in [0,1,2]){
+        let layerModifier = tileAmounts[0]/tileAmounts[l];
+        list.push(layerModifier)
+    }
+    return list;
+}
+
 function updateScreenDimensions(width,height){
     canvas.width = width;
     canvas.height = height;
 }
 
-function createBoard(layers,BoardDrawingData){ 
+function createBoard(layers,BoardData){ 
     for(let layer of layers){
         let tileAmount = layer.tiles;
         let radius = layer.radius;
         let ring = {
             type:'ringOutline',
-            x: 0,//canvas.width/2,
-            y: 0,//canvas.height/2,
+            x: 0,
+            y: 0,
             radius: radius,
         }
-        BoardDrawingData[layer.circleLayer+'Tiles'].push(ring);
+        BoardData[layer.circleLayer+'Tiles'].push(ring);
+        
         for(let step = 0; step < tileAmount; step++){  
+        
             let ang1 = 2*Math.PI * step/tileAmount;
-            let x =  radius*Math.cos(ang1); // canvas.width/2
-            let y =  radius*Math.sin(ang1); ///canvas.height/2 +
+            let x =  radius*Math.cos(ang1);
+            let y =  radius*Math.sin(ang1);
+           
             let tile = {
                 type:'tile',
                 x:x,
                 y:y,
                 angle:ang1,
+                tileType:'tile',
             }
-            BoardDrawingData[layer.circleLayer+'Tiles'].push(tile);
+            BoardData[layer.circleLayer+'Tiles'].push(tile);
     
         }
     }
+    let j = 0;
+    for(let i in BoardData['outerTiles']){
+        if(i == 0) continue; 
+        let tile = BoardData['outerTiles'][i];
+        if( (i-1)%(9) == 0 ){
+            tile.tileType = j+'Start';
+            j+=1;
+        }
+    }
    
-    return BoardDrawingData;
+    return BoardData;
 }
 
 
@@ -136,15 +157,37 @@ function draw(renderWorker){
     renderWorker.postMessage( 
         {
             data:[
-                ['drawBoard',BoardDrawingData,tileDimensions],
-                ['drawPlane',planes,planeDimensions],
+                ['drawBoard',BoardData,tileDimensions],
+                ['drawPlane',gameHandler.planes,planeDimensions],
             ]
         }
     )
 }
 
 function updatePlane(planes){
-    for(let p in planes){
+    /*for(let i in gameHandler.linInterpolations){
+        let linList = gameHandler.linInterpolations[i];
+        if(linList.length == 0) continue;
+        for(let j in linList){
+            let lins = linList[j];
+            if(lins == null || undefined) continue;
+            if(lins.time <= 0) 
+            {
+                gameHandler.linInterpolations[i][j] = null;
+                continue;
+            }
+           
+            lins.time -= debugHandler.delta;
+            planes[lins.plane][lins.key] = Util.linInterp(lins.a,lins.b,lins.time);
+            
+        }
+        
+    }*/
+
+    for(let p of planes){
+
+        p.angle += (Math.PI*2)/gameHandler.seccondsPerCycle*debugHandler.delta*(tileAmounts[0]/p.tilesInCycle)
+        p.angle = p.angle%(Math.PI*2)
         
     }
     return planes;
@@ -161,15 +204,23 @@ function update(renderWorker){
         debugHandler.frameBuffer.shift();  
     }
     debugHandler.frameBuffer.push(now);
-    updateRateDOM.innerHTML = "Update time: " + debugHandler.frameBuffer.length  ;
+    updateRateDOM.innerHTML = "FPS: " + debugHandler.frameBuffer.length  ;
 
-    debugHandler.timeSinceLastUpdate = currentTime;
-    planes = updatePlane(planes)
-    draw(renderWorker)
+    if(!gameHandler.pause)
+    {
+        gameHandler.planes = updatePlane(gameHandler.planes)
+        
+        gameHandler.timerValue += debugHandler.delta/gameHandler.seccondsPerCycle;
+        if(gameHandler.timerValue >= 1){
+            gameHandler.timerValue = 0;
+        }
+    }
     
-      
-     
-  
+    debugHandler.timeSinceLastUpdate = currentTime;
+    progressbarDOM.value = gameHandler.timerValue;
+    
+    draw(renderWorker)
+
 }
 
 window.onload = () =>{
@@ -177,19 +228,20 @@ window.onload = () =>{
     
     //set up
     updateScreenDimensions(1200,1200)
-    BoardDrawingData = createBoard(  [
+    BoardData = createBoard(  [
         {radius: 510, tiles:36, circleLayer:'outer' },
-         {radius: 340, tiles:18, circleLayer:'middle'},
-         {radius: 170, tiles:9, circleLayer:'inner'  }],
-          BoardDrawingData
+        {radius: 340, tiles:18, circleLayer:'middle'},
+        {radius: 170, tiles:9, circleLayer:'inner'  }],
+        BoardData
         );    
     
-    //console.log(BoardDrawingData['outerTiles'][1].x );
-    planes.x = BoardDrawingData['outerTiles'][1].x;  
-    planes.y = BoardDrawingData['outerTiles'][1].y; 
+    //console.log(BoardData['outerTiles'][1].x );
+ 
 
+    gameHandler.layerAngleSpeeds = calcLayerAngleSpeeds(tileAmounts,radiuses);
     var offscreen = canvas.transferControlToOffscreen();
     let renderWorker = new Worker('js/renderer.js'); 
+    
     renderWorker.postMessage( 
         {canvas: offscreen,
             data:[
@@ -202,9 +254,31 @@ window.onload = () =>{
         update(renderWorker);
     }
    
+    window.onkeydown = (e)=>{
+       gameHandler.keyStates[e] = 1;
 
-    //setInterval( ()=>{update(renderWorker)},2)
-    //setInterval( ()=>{draw(renderWorker)},1000/60)
+        switch(e.key){
+            case 'r':
+                for(let player of gameHandler.players){
+                    for(let p of player.planes){
+                        p.angle = 0;   
+                    }
+                }
+                gameHandler.timerValue = 0;
+                break;
+            case 'p':
+                gameHandler.pause = !gameHandler.pause;
+                break;
+            case 'f':
+                gameHandler.dispFPS = !gameHandler.dispFPS;
+                break;
+        }
+        
+    }
+
+    window.onkeyup = (e)=>{
+        gameHandler.keyStates[e] = 0;
+    }
 }
 
 
--- main/js/utility.js
+++ main/js/utility.js
@@ -9,4 +9,32 @@ export function getOffset( el ) {
     }
     return { top: _y, left: _x };
 }
+export function playerColors(index){
+    switch(index){
+        case 0 || '0':
+            return "#A416DB";
+        case 1 || '1':
+            return "#DB5F00"
+        case 2 || '2':
+            return "#16C4DB"
+        case 3 || '3':
+            return "#A2DB18";
+            
+    }
+}
 
+export function getSVGpath(modell){
+    switch(modell){
+        
+    }
+}
+
+export function linInterp(r1,r2,t){
+    let r = r1 + (r2-r1)*(1-t);
+    return r;
+}
+
+export function quadBezCurve(p0,p1,p2,t){
+    let r = p1 + (1-t)*(1-t)*(p0-p1) + t*t*(p2-p1);
+    return r;
+}
